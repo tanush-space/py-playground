dfdf7bd0603adc3659d47d7d7a79c3b8aa654eec
unknown <tanushgupta006work@gmail.com>
2025-10-04 22:34:25 +0530

    added practice 3 that is swapping of numbers

---
+++ practice/practice3.py
@@ -0,0 +1,18 @@
+'''swap two numbers'''
+'''
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+a, b = b, a
+print("After swapping: ")
+print("First number: " + a)
+print("Second number: " + b)
+'''
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+temp = a
+a = b
+b = temp
+print("After swapping: ")
+print("First number: " + a)
+print("Second number: " + b)
