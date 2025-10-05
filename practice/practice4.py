dab50685febccc2b9e0f01f90f124c17313023fd
unknown <tanushgupta006work@gmail.com>
2025-10-05 22:31:27 +0530

    added practice4.py

---
+++ practice/practice4.py
@@ -0,0 +1,14 @@
+'''take two numbers from user and display their sum'''
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))  
+# c = a + b   
+# print("Sum of two numbers is:" + str(c)   )
+
+# a = input("Enter first number: ")
+# b = input("Enter second number: ")
+# c = int(a) + int(b)
+# print("Sum of two numbers is:" + str(c)   )
+
+   
