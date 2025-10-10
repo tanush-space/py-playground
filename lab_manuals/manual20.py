03afe35ebc3ffe791264d8a22675250b1ed8a494
unknown <tanushgupta006work@gmail.com>
2025-10-10 23:51:04 +0530

    added lab manual file 20

---
+++ lab_manuals/manual20.py
@@ -0,0 +1,6 @@
+'''20	Write a program to create a tuple and access elements using indexing.'''
+t=tuple(input("Enter elements separated by space: ").split())
+print("First element:",t[0])
+print("Second element:",t[1])
+print("Last element:",t[-1])
+print("First three elements:",t[0:3])
